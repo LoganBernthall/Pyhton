4530d1bbb1fc06e1b928c2d4ad57e6a2c39b4b91
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2022-09-30 15:02:40 +0100

    -

--- Learn Numpy/NumpyLearning.py
+++ Learn Numpy/NumpyLearning.py
@@ -17,3 +17,28 @@ print("   ")#Adding whitespace only
 MultipliedArray = 2*Array1
 
 print(MultipliedArray)
+print("   ")#Adding whitespace only
+#Exercise 3 - Identity Matrix, creating an identity matrix of a 4 by 4 dimension
+
+IdentityMatrix = np.eye(4)
+print(IdentityMatrix)
+
+print("   ")#Adding whitespace only
+#Exercise 4 - Array re-dimensioning, converting a 1-D array to a 3-D array
+ReDimension = np.array([x for x in range(27)])
+ReShaped = ReDimension.reshape((3,3,3))
+print(ReShaped)
+
+#Exercise 5 - Array Datatype conversion, convert all the elements of a numpy array from float to integer 
+print("   ")#Adding whitespace only
+
+ArrayConversion = np.array([[2.5, 3.8, 1.5], [4.7,2.9,1.56]])
+ArrayConverted = ArrayConversion.astype('int')
+print(ArrayConverted)
+
+#Exercise 6 - Obtaining boolean array from binary array
+print("   ")#Adding whitespace only
+
+ArrayBoolean = np.array([[1,0,0], [1,1,1], [0,0,0]])
+ArrayBooleanSorted = ArrayBoolean.astype('bool')
+print(ArrayBooleanSorted)
