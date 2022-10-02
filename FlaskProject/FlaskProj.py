0690f23758b3a533561e48aaa47a0376eae508f1
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2022-10-02 14:12:11 +0100

    -

--- FlaskProject/FlaskProj.py
+++ FlaskProject/FlaskProj.py
@@ -1,7 +1,8 @@
+from pickle import NONE
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template('templates\index.html')
+def index(name=NONE):
+    return render_template('index.html', name=name)
