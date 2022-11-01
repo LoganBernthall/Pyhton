ba021e4ffccadf2736283acdbc20b8be4e7544f4
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2022-11-01 20:25:35 +0000

    -

--- Python Learning Project/Test.py
+++ Python Learning Project/Test.py
@@ -3,6 +3,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/index")
+def index():
     return render_template('index.html')
+
+@app.route("/basics")
+def basics():
+    return render_template('basics.html')
+
+@app.route("/uses")
+def uses():
+    return render_template('uses.html')
+    
+@app.route("/libraries")
+def libraries():
+    return render_template('libraries.html')
+
+@app.route("/credit")
+def credit():
+    return render_template('credit.html')
